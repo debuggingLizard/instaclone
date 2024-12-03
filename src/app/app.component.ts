40ef221e8c2a75668680968ca68327dd1b09f65e
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-12-03 14:56:13 +0100

    routing with contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainContentComponent},
+    {path: 'contacts', component: ContactPageComponent},
+];
